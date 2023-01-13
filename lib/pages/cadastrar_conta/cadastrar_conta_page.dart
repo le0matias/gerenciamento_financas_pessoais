fb4aa1763941a3f154c02ec91b0981e5889df4da
le0matias <leonardomfrancisco@gmail.com>
2023-01-13 15:42:34 -0300

    feat: validacao de dados cadastro conta

--- lib/pages/cadastrar_conta/cadastrar_conta_page.dart
+++ lib/pages/cadastrar_conta/cadastrar_conta_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gerenciamento_financas_pessoais/pages/home/home_page.dart';
 import 'package:gerenciamento_financas_pessoais/services/conta_rest_service.dart';
 import 'package:gerenciamento_financas_pessoais/services/conta_service.dart';
@@ -10,6 +11,7 @@ class CadastrarContaPage extends StatelessWidget {
   final _valorController = TextEditingController();
   ContaService cs = ContaService();
   ContaRestService crs = ContaRestService();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +24,7 @@ class CadastrarContaPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(10),
           child: Form(
+            key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -29,11 +32,26 @@ class CadastrarContaPage extends StatelessWidget {
                   controller: _nomeController,
                   keyboardType: TextInputType.text,
                   decoration: const InputDecoration(labelText: 'Nome'),
+                  validator: (value) {
+                    if(value == null || value.isEmpty) {
+                      return 'Preencha o campo Nome';
+                    } else {
+                      return null;
+                    }
+                  },
                 ),
                 TextFormField(
                   controller: _valorController,
                   keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                   decoration: const InputDecoration(labelText: 'Valor'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Preencha o campo Valor';
+                    } else {
+                      return null;
+                    }
+                  }
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 20, bottom: 20),
@@ -42,15 +60,17 @@ class CadastrarContaPage extends StatelessWidget {
                     width: double.infinity,
                     child: ElevatedButton(
                       onPressed: () {
-                        Conta novaConta = Conta(
-                          nome: _nomeController.text,
-                          valor: double.parse(_valorController.text),
-                        );
-                        // cs.adicionarConta(novaConta); sqlite method
-                        crs.addConta(novaConta);
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (_) => HomePage())
-                        );
+                        if(_formKey.currentState!.validate()) {
+                          Conta novaConta = Conta(
+                            nome: _nomeController.text,
+                            valor: double.parse(_valorController.text),
+                          );
+                          // cs.adicionarConta(novaConta); sqlite method
+                          crs.addConta(novaConta);
+                          Navigator.of(context).push(
+                              MaterialPageRoute(builder: (_) => HomePage())
+                          );
+                        }
                       },
                       child: const Text(
                         'Cadastrar',
